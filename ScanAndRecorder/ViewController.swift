4580a2a8b83a7b8d78af6030a54976109763050c
Margarita Chernyaeva <margarita.chernyaeva@itrexgroup.com>
2021-09-19 19:30:24 +0300

    Initial Commit

---
+++ ScanAndRecorder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScanAndRecorder
+//
+//  Created by Margarita Chernyaeva on 19.09.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
